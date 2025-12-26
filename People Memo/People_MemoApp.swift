963c57a46c4bb0fb72918451081e97b931f80b6e
Ynosky <Ryuryu317!>
2025-12-26 21:40:57 +0900

    Initial Commit

---
+++ People Memo/People_MemoApp.swift
@@ -0,0 +1,32 @@
+//
+//  People_MemoApp.swift
+//  People Memo
+//
+//  Created by ryunosuke sato on 2025/12/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct People_MemoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ People MemoTests/People_MemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  People_MemoTests.swift
+//  People MemoTests
+//
+//  Created by ryunosuke sato on 2025/12/26.
+//
+
+import Testing
+@testable import People_Memo
+
+struct People_MemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
